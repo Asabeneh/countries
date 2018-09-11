55f848e857a14925b76d5432e23e997329a567c1
Asabeneh123 <asabeneh@integrify.io>
2018-09-11 14:52:59 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,62 @@
+
+const countriesWrapper = document.querySelector('.countries-wrapper');
+let searchInput = document.querySelector('#search-text')
+let totalCountries =document.querySelector('#total-countries');
+const initialText = document.querySelector("#starts-with");
+const textWord = document.querySelector("#text-word");
+
+
+totalCountries.textContent =  'Total Number of countries ' + countries.length;
+
+const hexaColor = function() {
+    let numbersLetters = '0123456789abcdef'.split('');
+    let hexaNumbers = '';
+    let randIndex;
+    for (let i = 0; i < 6; i++) {
+    randIndex = Math.floor(Math.random() * numbersLetters.length);
+    hexaNumbers += numbersLetters[randIndex];
+    }
+    return '#' + hexaNumbers;
+};
+
+const displayCountries = (arr) => {
+    countriesWrapper.innerHTML = '';
+    let div;
+    arr.forEach((country) => {
+        div = document.createElement('div');
+        div.textContent = country;
+        div.className = 'country';
+        div.style.background = hexaColor();
+        div.style.color = hexaColor();
+        countriesWrapper.appendChild(div);
+    })
+}
+
+
+searchInput.addEventListener('input',() => {
+    let len = filterCountriesByInitials(searchInput.value).length;
+    len > 0  ? document.querySelector('#filtered-countries').textContent = `Countries starting with ${searchInput.value} are ${len}.` : ''
+    
+    displayCountries(filterCountriesByInitials(searchInput.value))
+})
+
+const filterCountriesByInitials = (searchText) => {
+    let search = searchText.slice(0,1).toUpperCase() + searchText.slice(1).toLowerCase();
+    const filteredCountries = countries.filter((country) => {
+        return country.includes(search);
+    });
+    return searchText === '' ? countries : filteredCountries;
+}
+
+const filterCountriesByWord = (searchText) => {
+    let search = searchText.toLowerCase();
+    console.log(search)
+    const filteredCountries = countries.filter((country) => {
+        return country.toLowerCase().includes(searchText);
+
+    });
+    return searchText === '' ? countries : filteredCountries;
+}
+
+displayCountries(countries);
+
