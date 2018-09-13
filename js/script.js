e41c00f613967ef92c24fe6528c1097a363acb9e
Asabeneh123 <asabeneh@integrify.io>
2018-09-13 16:13:36 +0300

    sorte feature added

--- js/script.js
+++ js/script.js
@@ -4,6 +4,53 @@ let searchInput = document.querySelector('#search-text')
 let totalCountries =document.querySelector('#total-countries');
 const initialTextButton = document.querySelector("#starts-with");
 const textWordButton = document.querySelector("#text-word");
+const sortButton = document.querySelector('#sort');
+
+
+sortButton.addEventListener('click',() => {
+    // let toggle = true;
+    // let icon = document.querySelector('i');
+
+    // if(toggle) {
+    //     icon.classList.remove('fa-sort-alpha-down');
+    //     icon.classList.add('fa-sort-alpha-up')
+    //     countries.reverse();
+    //     displayCountries(countries);
+    //     toggle = false;
+
+    // }
+    // else {
+    //     displayCountries(countries);
+    //     toggle = false;
+    //     icon.classList.remove('fa-sort-alpha-up');
+    //     icon.classList.add('fa-sort-alpha-down');
+
+    //     toggle = true;
+
+    // }
+
+  
+
+    let icon = document.querySelector('i');
+  if (icon.classList.contains('fa-sort-alpha-down')){
+        icon.classList.remove('fa-sort-alpha-down');
+        icon.classList.add('fa-sort-alpha-up')
+        let sortedCountries = [...countries];
+        sortedCountries.reverse();
+        displayCountries(sortedCountries);
+        
+    }
+    else {
+        displayCountries(countries);
+        icon.classList.remove('fa-sort-alpha-up');
+        icon.classList.add('fa-sort-alpha-down')
+    }
+
+
+    
+
+    console.log(icon);
+})
 
 
 totalCountries.textContent =  'Total Number of countries ' + countries.length;
@@ -60,6 +107,8 @@ searchInput.addEventListener('input',() => {
 })
 
 initialTextButton.addEventListener('click',() => {
+    initialTextButton.setAttribute('class','active')
+    textWordButton.classList.remove('active');
     searchInput.addEventListener('input',() => {
         let len = filterCountriesByInitials(searchInput.value).length;
         len > 0  ? document.querySelector('#filtered-countries').textContent = `Countries starting with ${searchInput.value} are ${len}.` : ''
@@ -69,6 +118,8 @@ initialTextButton.addEventListener('click',() => {
 });
 
 textWordButton.addEventListener('click', () => {
+    textWordButton.setAttribute('class','active');
+    initialTextButton.classList.remove('active')
     searchInput.addEventListener('input',() => {
         let len = filterCountriesByWord(searchInput.value).length;
         len > 0  ? document.querySelector('#filtered-countries').textContent = `Countries containing ${searchInput.value} are ${len}.` : ''
